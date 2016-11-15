a1d9409568fc488aa07daad7e386bba0282f728f
Galder Zamarreño <galder@zamarreno.com>
2016-11-15 17:49:27 +0100

    Add security SNI example

---
+++ src/main/java/org/infinispan/sandbox/client/EncryptSniClient.java
@@ -0,0 +1,33 @@
+package org.infinispan.sandbox.client;
+
+import org.infinispan.client.hotrod.RemoteCache;
+import org.infinispan.client.hotrod.RemoteCacheManager;
+import org.infinispan.client.hotrod.configuration.ConfigurationBuilder;
+
+/**
+ * To run, copy resources/trust/keystore_server.jks to server configuration
+ */
+public class EncryptSniClient {
+
+   public static void main(String[] args) {
+      ConfigurationBuilder clientBuilder = new ConfigurationBuilder();
+      clientBuilder
+         .addServer()
+            .host("127.0.0.1")
+            .port(11222)
+         .socketTimeout(3000)
+         .security()
+            .ssl()
+               .enabled(true)
+               .trustStoreFileName("src/main/resources/trust/truststore.jks")
+               .trustStorePassword("secret".toCharArray())
+               .sniHostName("host1");
+
+      RemoteCacheManager rcm = new RemoteCacheManager(clientBuilder.build());
+      RemoteCache<Object, Object> cache = rcm.getCache();
+      cache.put(1, "v1");
+      System.out.println(cache.get(1));
+      rcm.stop();
+   }
+
+}
