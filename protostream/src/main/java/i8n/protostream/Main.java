ff5c6b2405249dc00f9dec73ccf3452926c1a07e
Galder Zamarreño <galder@zamarreno.com>
2018-03-18 19:07:35 +0100

    Register auto-generated marshaller and proto file

--- protostream/src/main/java/i8n/protostream/Main.java
+++ protostream/src/main/java/i8n/protostream/Main.java
@@ -3,6 +3,7 @@ package i8n.protostream;
 import org.infinispan.client.hotrod.marshall.ProtoStreamMarshaller;
 import org.infinispan.commons.io.ByteBuffer;
 import org.infinispan.protostream.SerializationContext;
+import org.infinispan.protostream.annotations.ProtoSchemaBuilder;
 
 import java.io.IOException;
 
@@ -11,11 +12,20 @@ public class Main {
    public static void main(String[] args) throws Exception {
       final ProtoStreamMarshaller m = new ProtoStreamMarshaller();
 
+      final SerializationContext serialCtx = m.getSerializationContext();
+      ProtoSchemaBuilder protoSchemaBuilder = new ProtoSchemaBuilder();
+      protoSchemaBuilder
+         .fileName("player.proto")
+         .addClass(Player.class)
+         .build(serialCtx);
+
       final Player p = new Player("Clemont", 10);
+      System.out.println("[write] " + p);
       final byte[] bytes = m.objectToByteBuffer(p);
 
       final Player pp = (Player) m.objectFromByteBuffer(bytes);
-      System.out.println(p.equals(pp));
+      System.out.println("[read]  " + p);
+      System.out.println("Equals? " + p.equals(pp));
    }
 
 }
