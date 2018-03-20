705084d1b69f3a1c2e7106c1efe2fe993d46ff8e
Galder Zamarreño <galder@zamarreno.com>
2018-03-20 14:35:37 +0100

    Loading Infinispan using JBoss Modules

---
+++ parallel-stacks/src/main/java/i8n/pstacks/StacksMain.java
@@ -0,0 +1,30 @@
+package i8n.pstacks;
+
+import org.jboss.modules.LocalModuleLoader;
+import org.jboss.modules.Module;
+import org.jboss.modules.ModuleIdentifier;
+import org.jboss.modules.ModuleLoader;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class StacksMain {
+
+   public static void main(String[] args) throws Exception {
+      final File repoRoot = ModulesUtil.getResourceFile(StacksMain.class, "test/repo");
+      ModuleLoader moduleLoader = new LocalModuleLoader(new File[]{repoRoot});
+
+      final Path tmpdir = Files.createTempDirectory("repository");
+
+      System.setProperty("maven.repo.local", tmpdir.toAbsolutePath().toString());
+      System.setProperty("remote.maven.repo", "http://repository.jboss.org/nexus/content/groups/public/");
+
+      ModuleIdentifier moduleId = ModuleIdentifier.fromString("sample.maven");
+      Module module = moduleLoader.loadModule(moduleId);
+
+      final Class<?> clazz = module.getClassLoader().loadClass("org.infinispan.commons.api.BasicCache");
+      System.out.println(clazz);
+   }
+
+}
