261f2b38c7d4683e11c08b5f5ea70f85fa81b982
Galder Zamarreño <galder@zamarreno.com>
2018-09-28 07:34:50 +0200

    Replicating cluster failover issue

---
+++ cluster-failover/src/main/java/org/infinispan/clusterfailover/NamedCache.java
@@ -0,0 +1,27 @@
+package org.infinispan.clusterfailover;
+
+import org.infinispan.client.hotrod.RemoteCache;
+import org.infinispan.client.hotrod.RemoteCacheManager;
+import org.infinispan.client.hotrod.configuration.ConfigurationBuilder;
+
+public class NamedCache {
+
+   public static void main(String[] args) {
+      ConfigurationBuilder builder = new ConfigurationBuilder();
+
+      builder
+         .addServer()
+            .host("localhost").port(11332) // site1-node1
+         .addServer()
+            .host("localhost").port(11342) // site1-node2
+         .addCluster("site2")
+            .addClusterNode("localhost", 11432) // site2-node1
+            .addClusterNode("localhost", 11442); // site2-node2
+
+      RemoteCacheManager remote = new RemoteCacheManager(builder.build());
+
+      RemoteCache<String, String> cache = remote.getCache("repl");
+      cache.put("a", "a");
+   }
+
+}
