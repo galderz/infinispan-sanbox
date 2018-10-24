420383ba8fea0550645c289ddd23823ec98b41cd
Galder Zamarreño <galder@zamarreno.com>
2018-10-24 09:30:22 +0200

    Populate a persistent index

--- varia/src/main/java/org/infinispan/sandbox/client/query/CreatePersistentIndex.java
+++ varia/src/main/java/org/infinispan/sandbox/client/query/CreatePersistentIndex.java
@@ -1,8 +1,19 @@
 package org.infinispan.sandbox.client.query;
 
+import junit.framework.Assert;
+import org.infinispan.client.hotrod.RemoteCache;
 import org.infinispan.client.hotrod.RemoteCacheManager;
+import org.infinispan.client.hotrod.Search;
+import org.infinispan.client.hotrod.configuration.ConfigurationBuilder;
+import org.infinispan.client.hotrod.marshall.ProtoStreamMarshaller;
 import org.infinispan.commons.api.CacheContainerAdmin;
 import org.infinispan.commons.configuration.XMLStringConfiguration;
+import org.infinispan.protostream.FileDescriptorSource;
+import org.infinispan.protostream.SerializationContext;
+import org.infinispan.query.dsl.Query;
+import org.infinispan.query.dsl.QueryFactory;
+import org.infinispan.query.remote.client.MarshallerRegistration;
+import org.infinispan.query.remote.client.ProtobufMetadataManagerConstants;
 
 import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Source;
@@ -10,22 +21,55 @@ import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
+import java.io.IOException;
 import java.io.StringReader;
 import java.io.StringWriter;
+import java.util.List;
 
-public class PersistIndex {
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
+
+public class CreatePersistentIndex {
+
+   private static final String CUSTOM_ANALYZER_PROTO_SCHEMA = "package sample_bank_account;\n" +
+      "/* @Indexed \n" +
+      "   @Analyzer(definition = \"standard-with-stop\") */" +
+      "message AnalyzerTestEntity {\n" +
+      "\t/* @Field(store = Store.YES, analyze = Analyze.YES, analyzer = @Analyzer(definition = \"stemmer\")) */\n" +
+      "\toptional string f1 = 1;\n" +
+      "\t/* @Field(store = Store.YES, analyze = Analyze.NO, indexNullAs = \"-1\") */\n" +
+      "\toptional int32 f2 = 2;\n" +
+      "}\n";
 
    public static void main(String[] args) {
       String dataCacheName = "persistent-data";
 
-      RemoteCacheManager cacheContainer = new RemoteCacheManager();
+      ConfigurationBuilder cfg = new ConfigurationBuilder();
+      cfg.marshaller(new ProtoStreamMarshaller());
+      RemoteCacheManager cacheContainer = new RemoteCacheManager(cfg.build());
+
+      initProtoSchema(cacheContainer);
 
       createIndexLockingCache(cacheContainer);
       createIndexMetadataCache(cacheContainer);
       createIndexDataCache(cacheContainer);
       createDataCache(dataCacheName, cacheContainer);
 
-      
+      final RemoteCache<String, AnalyzerTestEntity> cache = cacheContainer.getCache(dataCacheName);
+      assertNotNull(cache);
+
+      cache.put("analyzed1", new AnalyzerTestEntity("tested 123", 3));
+      cache.put("analyzed2", new AnalyzerTestEntity("testing 1234", 3));
+      cache.put("analyzed3", new AnalyzerTestEntity("xyz", null));
+
+      final QueryFactory queryFactory = Search.getQueryFactory(cache);
+      final Query query = queryFactory
+         .create("from sample_bank_account.AnalyzerTestEntity where f1:'test'");
+      List<AnalyzerTestEntity> list = query.list();
+
+      System.out.printf("Query returned: %s%n", list);
+
+      assertEquals(2, list.size());
    }
 
    private static void createDataCache(String dataCacheName, RemoteCacheManager cacheContainer) {
@@ -128,6 +172,41 @@ public class PersistIndex {
          return xmlOutput.getWriter().toString();
       } catch (Exception e) {
          throw new RuntimeException(e); // simple exception handling, please review it
+      }
+   }
+
+   public static void initProtoSchema(RemoteCacheManager remoteCacheManager) {
+      // initialize server-side serialization context
+      RemoteCache<String, String> metadataCache = remoteCacheManager.getCache(ProtobufMetadataManagerConstants.PROTOBUF_METADATA_CACHE_NAME);
+      metadataCache.put("custom_analyzer.proto", CUSTOM_ANALYZER_PROTO_SCHEMA);
+      checkSchemaErrors(metadataCache);
+
+      // initialize client-side serialization context
+      try {
+         SerializationContext serCtx = ProtoStreamMarshaller.getSerializationContext(remoteCacheManager);
+         MarshallerRegistration.registerMarshallers(serCtx);
+         serCtx.registerProtoFiles(FileDescriptorSource.fromString("custom_analyzer.proto", CUSTOM_ANALYZER_PROTO_SCHEMA));
+         serCtx.registerMarshaller(new AnalyzerTestEntityMarshaller());
+      } catch (IOException e) {
+         throw new AssertionError(e);
+      }
+   }
+
+   /**
+    * Logs the Protobuf schema errors (if any) and fails the test if there are schema errors.
+    */
+   public static void checkSchemaErrors(RemoteCache<String, String> metadataCache) {
+      if (metadataCache.containsKey(ProtobufMetadataManagerConstants.ERRORS_KEY_SUFFIX)) {
+         // The existence of this key indicates there are errors in some files
+         String files = metadataCache.get(ProtobufMetadataManagerConstants.ERRORS_KEY_SUFFIX);
+         for (String fname : files.split("\n")) {
+            String errorKey = fname + ProtobufMetadataManagerConstants.ERRORS_KEY_SUFFIX;
+            System.err.println(String.format(
+               "Found errors in Protobuf schema file: %s\n%s\n", fname, metadataCache.get(errorKey))
+            );
+         }
+
+         Assert.fail("There are errors in the following Protobuf schema files:\n" + files);
       }
    }
 
