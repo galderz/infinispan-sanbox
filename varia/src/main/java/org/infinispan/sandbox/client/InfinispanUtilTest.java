a1c098659e12e8f34253a586095627f43d53f406
Galder Zamarreño <galder@zamarreno.com>
2018-10-25 11:39:10 +0200

    More focused functions

--- varia/src/main/java/org/infinispan/sandbox/client/InfinispanUtilTest.java
+++ varia/src/main/java/org/infinispan/sandbox/client/InfinispanUtilTest.java
@@ -1,6 +1,6 @@
 package org.infinispan.sandbox.client;
 
-import org.infinispan.client.hotrod.RemoteCache;
+import org.infinispan.client.hotrod.RemoteCacheManager;
 import org.infinispan.client.hotrod.configuration.ConfigurationBuilder;
 import org.infinispan.sandbox.client.InfinispanUtil.LazyRemoteCacheManager;
 
@@ -8,17 +8,18 @@ public class InfinispanUtilTest {
 
    public static void main(String[] args) {
       try (LazyRemoteCacheManager lazyRemote = InfinispanUtil.lazyRemoteCacheManager()) {
-         lazyRemote.andThen(remote -> {
-            final RemoteCache<Object, Object> cache = remote.getCache();
-            cache.put("k1", "v1");
-            return cache;
-         })
-         .andThen(cache -> {
-            final Object v = cache.get("k1");
-            System.out.println(v);
-            return cache;
-         })
-         .apply(new ConfigurationBuilder());
+         lazyRemote
+            .andThen(RemoteCacheManager::getCache)
+            .andThen(cache -> {
+               cache.put("k1", "v1");
+               return cache;
+            })
+            .andThen(cache -> {
+               final Object v = cache.get("k1");
+               System.out.println(v);
+               return cache;
+            })
+            .apply(new ConfigurationBuilder());
       }
    }
 
