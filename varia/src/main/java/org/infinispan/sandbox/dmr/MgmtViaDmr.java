1590a2b7462626405802a9371b4bd9aadabcd6fa
Galder Zamarreño <galder@zamarreno.com>
2018-10-29 13:00:27 +0100

    Using creaper (DMR) to extract information from server

---
+++ varia/src/main/java/org/infinispan/sandbox/dmr/MgmtViaDmr.java
@@ -0,0 +1,37 @@
+package org.infinispan.sandbox.dmr;
+
+import org.wildfly.extras.creaper.core.ManagementClient;
+import org.wildfly.extras.creaper.core.online.Constants;
+import org.wildfly.extras.creaper.core.online.ModelNodeResult;
+import org.wildfly.extras.creaper.core.online.OnlineManagementClient;
+import org.wildfly.extras.creaper.core.online.OnlineOptions;
+import org.wildfly.extras.creaper.core.online.operations.Address;
+import org.wildfly.extras.creaper.core.online.operations.Operations;
+
+import java.io.IOException;
+
+public class MgmtViaDmr {
+
+   private static final String NODE0_ADDRESS = System.getProperty("node0.ip", "127.0.0.1");
+   private static final int NODE0_PORT = Integer.valueOf(System.getProperty("node0.mgmt.port", "9990"));
+   private static final String LOGIN = System.getProperty("login", "admin");
+   private static final String PASSWORD = System.getProperty("password", "admin9Pass!");
+
+   public static void main(String[] args) throws IOException {
+      OnlineManagementClient onlineClient =
+         ManagementClient.online(
+            OnlineOptions.standalone()
+               .hostAndPort(NODE0_ADDRESS, NODE0_PORT)
+               .auth(LOGIN, PASSWORD)
+               .build()
+      );
+
+      final Operations ops = new Operations(onlineClient);
+
+      ModelNodeResult result = ops.readAttribute(Address.root(), Constants.SERVER_STATE);
+      result.assertDefinedValue();
+
+      System.out.println("Server state is: " + result.stringValue());
+   }
+
+}
