4ac6f75e782a04c9e90deb8600a39f0a0ede7154
Galder Zamarreño <galder@zamarreno.com>
2018-10-30 17:32:53 +0100

    Extract number of owners from command line output

--- varia/src/main/java/org/infinispan/sandbox/management/MgmtViaCmdLine.java
+++ varia/src/main/java/org/infinispan/sandbox/management/MgmtViaCmdLine.java
@@ -1,5 +1,8 @@
 package org.infinispan.sandbox.management;
 
+import org.jboss.dmr.ModelNode;
+import org.wildfly.extras.creaper.core.online.ModelNodeResult;
+
 public class MgmtViaCmdLine {
 
    public static void main(String[] args) {
@@ -9,7 +12,17 @@ public class MgmtViaCmdLine {
          "oc exec -it datagrid-service-0 -- /opt/datagrid/bin/cli.sh --connect --commands=/subsystem=datagrid-infinispan/cache-container=clustered/configurations=CONFIGURATIONS/distributed-cache-configuration=default:read-attribute(name=owners)"
       );
 
-      System.out.println(result);
+      if (result.getErrorCode() == 0) {
+         System.out.println(result);
+
+         final ModelNodeResult numOwners = new ModelNodeResult(ModelNode.fromString(result.getOutput()));
+         numOwners.assertDefinedValue();
+
+         System.out.println("Num owners is: " + numOwners.stringValue());
+      } else {
+         System.out.println("Error executing command: " + result);
+      }
+
    }
 
 }
