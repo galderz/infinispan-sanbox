bd8f2331dac953dd3b7a36ae87420757af56d4ae
Galder Zamarreño <galder@zamarreno.com>
2018-10-30 17:25:56 +0100

    Remove quotes

--- varia/src/main/java/org/infinispan/sandbox/management/MgmtViaCmdLine.java
+++ varia/src/main/java/org/infinispan/sandbox/management/MgmtViaCmdLine.java
@@ -6,7 +6,7 @@ public class MgmtViaCmdLine {
       final CommandlineClient cmdLine = new CommandlineClient();
 
       final CommandlineClient.Result result = cmdLine.invokeAndReturnResult(
-         "oc exec -it datagrid-service-0 -- /opt/datagrid/bin/cli.sh --connect --commands=\"/subsystem=datagrid-infinispan/cache-container=clustered/configurations=CONFIGURATIONS/distributed-cache-configuration=default:read-attribute(name=owners)\""
+         "oc exec -it datagrid-service-0 -- /opt/datagrid/bin/cli.sh --connect --commands=/subsystem=datagrid-infinispan/cache-container=clustered/configurations=CONFIGURATIONS/distributed-cache-configuration=default:read-attribute(name=owners)"
       );
 
       System.out.println(result);
