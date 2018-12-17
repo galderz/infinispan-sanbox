b09d6cbfd591ac5508627ced0c3f1fc6c86e1654
Galder Zamarreño <galder@zamarreno.com>
2018-12-17 11:30:43 +0100

    Not interactive, update cli script location

--- varia/src/main/java/org/infinispan/sandbox/management/MgmtViaOpenShiftCmdLine.java
+++ varia/src/main/java/org/infinispan/sandbox/management/MgmtViaOpenShiftCmdLine.java
@@ -33,8 +33,8 @@ public class MgmtViaOpenShiftCmdLine {
          .apply(
             String.format(
                "oc exec " +
-                  "-it %s " +
-                  "-- /opt/datagrid/bin/cli.sh " +
+                  "%s " +
+                  "-- /opt/datagrid/bin/ispn-cli.sh " +
                   "--connect " +
                   "--commands=/subsystem=datagrid-infinispan/cache-container=clustered/configurations=CONFIGURATIONS/distributed-cache-configuration=default:read-attribute(name=owners)"
                , podName
@@ -50,8 +50,8 @@ public class MgmtViaOpenShiftCmdLine {
          .apply(
             String.format(
                "oc exec " +
-                  "-it %s " +
-                  "-- /opt/datagrid/bin/cli.sh " +
+                  "%s " +
+                  "-- /opt/datagrid/bin/ispn-cli.sh " +
                   "--connect " +
                   "--commands=/subsystem=datagrid-infinispan/cache-container=clustered/configurations=CONFIGURATIONS/distributed-cache-configuration=default:read-attribute(name=owners)"
                , podName
